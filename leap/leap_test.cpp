bc49b8368da3f62f482d77f5aa51b07e264d6a87
Egor Kostan <igorkostan@gmail.com>
2020-01-11 16:15:04 -0800

    Once you've done that, uncomment the next test by moving the following line past the next test.

--- leap/leap_test.cpp
+++ leap/leap_test.cpp
@@ -1,12 +1,12 @@
-#include "leap.h"
-#include "test/catch.hpp"
+include "leap.h"
+include "test/catch.hpp"
 
 TEST_CASE("not_divisible_by_4")
 {
     REQUIRE(!leap::is_leap_year(2015));
 }
 
-#if defined(EXERCISM_RUN_ALL_TESTS)
+if defined(EXERCISM_RUN_ALL_TESTS)
 TEST_CASE("divisible_by_2_not_divisible_by_4")
 {
     REQUIRE(!leap::is_leap_year(1970));
