93015582d6310efdbaf50fc99df6f80d496381c8
Egor Kostan <igorkostan@gmail.com>
2020-01-13 11:49:54 -0800

    Update leap.cpp

--- leap/leap.cpp
+++ leap/leap.cpp
@@ -7,6 +7,6 @@ namespace leap {
 		//on every year that is evenly divisible by 4
 		//except every year that is evenly divisible by 100
 		//unless the year is also evenly divisible by 400
-		return (year % 4 == 0 && year % 100 != 0) || (year % 100 == 0 && year % 400 == 0);	
+		return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);	
 	}
 }  // namespace leap
