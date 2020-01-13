35b312356811b8ae7a253cd51694214b2678b4c4
Egor Kostan <igorkostan@gmail.com>
2020-01-13 11:47:07 -0800

    Update leap.cpp

--- leap/leap.cpp
+++ leap/leap.cpp
@@ -5,24 +5,8 @@ namespace leap {
 	bool is_leap_year(int year) {
 
 		//on every year that is evenly divisible by 4
-		if (year % 4 == 0) {
-
-			//except every year that is evenly divisible by 100
-			if (year % 100 == 0) {
-
-				//unless the year is also evenly divisible by 400
-				if (year % 400 == 0) {
-					return true;
-				}
-				return false;
-			}
-			else {
-				return true;
-			};
-		}
-		else {
-			return false;
-		}
-	
+		//except every year that is evenly divisible by 100
+		//unless the year is also evenly divisible by 400
+		return (year % 4 == 0 && year % 100 != 0) || (year % 100 == 0 && year % 400 == 0);	
 	}
 }  // namespace leap
